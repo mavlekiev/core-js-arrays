a80e38aedd415cdcf991d3ca3953ed294474b67b
mavlekiev <mavlekiev@gmail.com>
2024-12-18 20:47:53 +0700

    init: add 2 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,13 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length > arr2.length) {
+    const arrCopy = arr1.slice(arr2.length, arr1.length);
+    return arr2.map((item, i) => item + arr1[i]).concat(arrCopy);
+  }
+  const arrCopy = arr2.slice(arr1.length, arr2.length);
+  return arr1.map((item, i) => item + arr2[i]).concat(arrCopy);
 }
 
 /**
